59599796a9d17ec8f78f2f163012e0e02debb662
coopeaus <coopeaus@oregonstate.edu>
2024-11-11 19:32:13 -0500

    Added automated formatting and linting checks.
    removed intentional failures. added automated formatting and linting checks
    
    removed compilation_commands.json so that the linter would not use it
    
    added step to create build directory for clang-tidy
    
    removed compile-commands database due to linter trying to check build directory
    
    bump to verison 18
    
    added verbosity to debug linting issue
    
    added quotations for cross-compatability and CI usage
    
    added ./compile_commands database to the workflow
    
    moved compile_commands out
    
    added more bad code to test linting checks

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,8 @@
-
 #include <iostream>
+
 
 int main()
 {
     std::cout << "Hello, World!" << '\n';
     return 0;
 }
-
-int NonSENSEFUNC()
-{
-    // Generate some linting and formatting errors
-    size_t IncorrectVariableName = 0;
-    return 0;
-    }
