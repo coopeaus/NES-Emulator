108e2e1d09061cb416502a5450db6643a0418021
coopeaus <coopeaus@oregonstate.edu>
2024-11-11 19:04:33 -0500

    generate some linting errors to test the action

--- src/main.cpp
+++ src/main.cpp
@@ -6,3 +6,10 @@ int main()
     std::cout << "Hello, World!" << '\n';
     return 0;
 }
+
+int NonSENSEFUNC()
+{
+    // Generate some linting and formatting errors
+    size_t IncorrectVariableName = 0;
+    return 0;
+    }
