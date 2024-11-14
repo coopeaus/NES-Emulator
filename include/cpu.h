91be65779518659a7969d4dc0d0c825063a1fcd8
Anthony Amador <55220361+AnthonySchool@users.noreply.github.com>
2024-11-13 20:57:35 -0800

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- include/cpu.h
+++ include/cpu.h
@@ -15,33 +15,33 @@ class CPU
     explicit CPU( Bus *bus ); // Must pass a pointer to a Bus class on initialization
 
     // Read/write methods
-    [[nodiscard]] auto read(u16 address) const -> u8;
-    void write(u16 address, u8 data);
+    [[nodiscard]] auto read( u16 address ) const -> u8;
+    void               write( u16 address, u8 data );
 
     // Getters for registers
-    [[nodiscard]] u8 getAccumulator() const;
-    [[nodiscard]] u8 getXRegister() const;
-    [[nodiscard]] u8 getYRegister() const;
-    [[nodiscard]] u8 getStatusRegister() const;
-    [[nodiscard]] u8 getStackPointer() const;
-    [[nodiscard]] u16 getProgramCounter() const;
+    [[nodiscard]] u8  GetAccumulator() const;
+    [[nodiscard]] u8  GetXRegister() const;
+    [[nodiscard]] u8  GetYRegister() const;
+    [[nodiscard]] u8  GetStatusRegister() const;
+    [[nodiscard]] u8  GetStackPointer() const;
+    [[nodiscard]] u16 GetProgramCounter() const;
 
     // Setters for registers
-    void setAccumulator(u8 value);
-    void setXRegister(u8 value);
-    void setYRegister(u8 value);
-    void setStatusRegister(u8 value);
-    void setStackPointer(u8 value);
-    void setProgramCounter(u16 value);
+    void SetAccumulator( u8 value );
+    void SetXRegister( u8 value );
+    void SetYRegister( u8 value );
+    void SetStatusRegister( u8 value );
+    void SetStackPointer( u8 value );
+    void SetProgramCounter( u16 value );
 
   private:
-    Bus* _bus; // Pointer to the Bus class
+    Bus *_bus; // Pointer to the Bus class
 
     // Registers
     u16 _pc = 0x0000; // Program counter (PC)
-    u8 _a = 0x00;     // Accumulator register (A)
-    u8 _x = 0x00;     // X register
-    u8 _y = 0x00;     // Y register
-    u8 _s = 0xFF;     // Stack pointer (SP)
-    u8 _p = 0x00;     // Status register (P)
+    u8  _a = 0x00;    // Accumulator register (A)
+    u8  _x = 0x00;    // X register
+    u8  _y = 0x00;    // Y register
+    u8  _s = 0xFF;    // Stack pointer (SP)
+    u8  _p = 0x00;    // Status register (P)
 };
