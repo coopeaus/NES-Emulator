1b91dd2412ebead1bd7eb5505f81173ed72a5b2b
William "Cody" Hill <80869677+WillCoHill@users.noreply.github.com>
2024-11-16 16:59:17 -0800

    Update src/cpu.cpp
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -30,4 +30,4 @@ void CPU::SetCycles( u64 value ) { _cycles = value; }
 void CPU::SetFlag( const u8 flag ) { _p |= flag; }
 void CPU::ClearFlag( const u8 flag ) { _p &= ~flag; }
 
-u8 CPU::IsFlagSet( const u8 flag ) const { return static_cast<u8>((_p & flag) != 0);}
+u8 CPU::IsFlagSet( const u8 flag ) const { return static_cast<u8>( ( _p & flag ) != 0 ); }
