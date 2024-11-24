fc78adc8adeef1e2d08c18ea10c9272ce8dc9663
WillCoHill <cody.silas.hill@gmail.com>
2024-11-24 11:37:15 -0800

    Added flag set and clear methods for specific registers

--- include/cpu.h
+++ include/cpu.h
@@ -50,10 +50,21 @@ class CPU
     void SetProgramCounter( u16 value );
     void SetCycles( u64 value );
 
-  //Flag methods
+    //Flag methods
 
     void SetFlag( u8 flag );
     void ClearFlag( u8 flag );
+
+  //Clear for Flags
+    void CLC() const;
+    void CLI() const;
+    void CLD() const;
+    void CLV() const;
+
+  //Setters for Flags
+    void SEC();
+    void SED();
+    void SEI();
     [[nodiscard]] u8   IsFlagSet( u8 flag ) const;
 
   private:
