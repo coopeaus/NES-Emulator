598cd19ad0ae88f9d61d607e619d31f8eac38b9a
Aizenhow <114388054+Aizenhow@users.noreply.github.com>
2025-04-18 23:21:08 +0000

    added slight fixes and linting

--- core/mappers/mapper3.cpp
+++ core/mappers/mapper3.cpp
@@ -1,21 +1,22 @@
 #include "mapper3.h"
+#include "cartridge-header.h"
+#include "mappers/mapper-base.h"
+#include "global-types.h"
 
-Mapper3::Mapper3(iNes2Instance iNesHeader)
-    : Mapper(iNesHeader)
+Mapper3::Mapper3( iNes2Instance iNesHeader ) : Mapper( iNesHeader ), _chrBank(0)
 {
-    _chrBank = 0;
+    
 }
 
-u32 Mapper3::TranslateCPUAddress(u16 address)
+u32 Mapper3::TranslateCPUAddress( u16 address )
 {
     // CNROM: PRG is fixed, typically 32KB at $8000-$FFFF
     // Map $8000-$FFFF to PRG-ROM directly
-    if (address >= 0x8000)
-    {
+    if ( address >= 0x8000 ) {
         // If only 16KB PRG, mirror it
-        int prgBankCount = GetPrgBankCount();
+        int const prgBankCount = GetPrgBankCount();
         u32 prgAddr = address - 0x8000;
-        if (prgBankCount == 1) // 16KB
+        if ( prgBankCount == 1 ) // 16KB
             prgAddr = prgAddr % 0x4000;
         return prgAddr;
     }
@@ -23,33 +24,30 @@ u32 Mapper3::TranslateCPUAddress(u16 address)
     return 0;
 }
 
-u32 Mapper3::TranslatePPUAddress(u16 address)
+u32 Mapper3::TranslatePPUAddress( u16 address )
 {
     // CNROM: CHR is banked in 8KB units
-    if (address < 0x2000)
-    {
-        u32 chrBankCount = GetChrBankCount();
-        u32 bank = _chrBank % chrBankCount;
-        return (bank * 0x2000) + address;
+    if ( address < 0x2000 ) {
+        u32 const chrBankCount = GetChrBankCount();
+        u32 const bank = _chrBank % chrBankCount;
+        return ( bank * 0x2000 ) + address;
     }
     // Not CHR address
     return address;
 }
 
-void Mapper3::HandleCPUWrite(u16 address, u8 data)
+void Mapper3::HandleCPUWrite( u16 address, u8 data )
 {
     // Any write to $8000-$FFFF sets CHR bank
-    if (address >= 0x8000 && address <= 0xFFFF)
-    {
+    if ( address >= 0x8000 && address <= 0xFFFF ) {
         // Use a mask based on available CHR banks (support up to 8 banks)
-        u8 mask = (GetChrBankCount() > 0) ? (GetChrBankCount() - 1) : 0x03;
+        u8 const mask = ( GetChrBankCount() > 0 ) ? ( GetChrBankCount() - 1 ) : 0x03;
         _chrBank = data & mask;
     }
 }
 
-
 MirrorMode Mapper3::GetMirrorMode()
 {
     // Use mirroring from header
-    return iNes.GetMirrorMode();
+    return static_cast<MirrorMode>( iNes.GetMirroring() );
 }
