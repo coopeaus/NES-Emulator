fee214bc6fca19f2aa154b66c7ae538867175e6c
coopeaus <coopeaus@oregonstate.edu>
2025-05-05 21:41:19 -0400

    added includes to blip_buffer.cpp

--- lib/Nes_Snd_Emu/nes_apu/Blip_Buffer.cpp
+++ lib/Nes_Snd_Emu/nes_apu/Blip_Buffer.cpp
@@ -6,6 +6,8 @@
 
 #include <string.h>
 #include <math.h>
+#include <limits>
+#include <new>
 
 /* Copyright (C) 2003-2005 Shay Green. This module is free software; you
 can redistribute it and/or modify it under the terms of the GNU Lesser
